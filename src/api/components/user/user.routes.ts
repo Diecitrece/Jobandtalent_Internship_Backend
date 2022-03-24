998c09d853919166bd71e03612632bbe0bf0b8fe
juanfril <juanfril@gmail.com>
2022-03-24 12:47:14 +0100

    feat: add method for generate ids

--- src/api/components/user/user.routes.ts
+++ src/api/components/user/user.routes.ts
@@ -2,8 +2,15 @@ import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { Router } from 'express';
 import { User } from './model';
+import { v4 as uuidv4 } from 'uuid';
 
 const usersInMemory: User[] = [];
+let id = '';
+
+const generateId = (): string => {
+  id = uuidv4();
+  return id;
+};
 
 const router = Router();
 router.use(bodyParser.json());
@@ -13,6 +20,8 @@ router.get('/api/users', (req: Request, res: Response) => {
 });
 
 router.post('/api/users', (req: Request, res: Response) => {
+  const user = req.body;
+  user.id = generateId();
   usersInMemory.push(req.body);
   res.json(req.body);
 });
