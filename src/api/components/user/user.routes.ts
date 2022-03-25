7dad35b5479613bec6f35fffe2f6de4e31b66538
juanfril <juanfril@gmail.com>
2022-03-25 13:48:25 +0100

    refactor: link user with schema remove unnecessary if statement valitation all mails return whole object

--- src/api/components/user/user.routes.ts
+++ src/api/components/user/user.routes.ts
@@ -12,20 +12,18 @@ const generateId = (): string => uuidv4();
 const router = Router();
 router.use(bodyParser.json());
 
-router.get('/api/users', (res: Response) => {
+router.get('/api/users', (req: Request, res: Response) => {
   res.json(usersInMemory);
 });
 
 router.post('/api/users', (req: Request, res: Response) => {
   const user: User = req.body;
   if (schemaUserRegister.validate(user).error)
-    res.send(schemaUserRegister.validate(user).error?.details);
+    return res.send(schemaUserRegister.validate(user).error?.details);
 
-  if (!schemaUserRegister.validate(user).error) {
-    res.send(schemaUserRegister.validate(user));
-    user.id = generateId();
-    usersInMemory.push(user);
-  }
+  user.id = generateId();
+  res.json(user);
+  usersInMemory.push(user);
 });
 
 export default router;
