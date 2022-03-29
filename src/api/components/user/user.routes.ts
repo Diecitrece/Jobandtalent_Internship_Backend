1d1cdba40735a31187026d418568f25f2e162669
juanfril <juanfril@gmail.com>
2022-03-29 22:49:24 +0200

    feat: create db conection

--- src/api/components/user/user.routes.ts
+++ src/api/components/user/user.routes.ts
@@ -2,31 +2,34 @@ import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { Router } from 'express';
 import { User } from './model';
-import { v4 as uuidv4 } from 'uuid';
 import sendEmail from './services/mail';
 import { schemaUserRegister } from './services/validations/user_register.validation';
-
-const usersInMemory: User[] = [];
-
-const generateId = (): string => uuidv4();
+import { db } from '../../../database/db-conection';
 
 const router = Router();
 router.use(bodyParser.json());
 
-router.get('/api/users', (req: Request, res: Response) => {
-  res.json(usersInMemory);
+router.get('/api/users', async (req: Request, res: Response) => {
+  const users = await db<User[]>('users').select('*');
+  res.json(users);
 });
 
-router.post('/api/users', (req: Request, res: Response) => {
+router.post('/api/users', async (req: Request, res: Response) => {
   const user: User = req.body;
   if (schemaUserRegister.validate(user).error) {
     return res.send(schemaUserRegister.validate(user).error?.details);
   }
-
-  user.id = generateId();
-  sendEmail(user);
-  res.send(user);
-  usersInMemory.push(user);
+  const userExists = await db<User>('users')
+    .where({ email: user.email })
+    .first();
+  if (userExists) {
+    return res.send('User already exists');
+  }
+  const userCreated = await db<User>('users').insert(user);
+  if (userCreated) {
+    sendEmail(user);
+    return res.json(userCreated);
+  }
 });
 
 export default router;
--- src/database/db-conection.ts
+++ src/database/db-conection.ts
@@ -1,26 +1,4 @@
-import knexConnection from 'knex';
-const knexConnect = knexConnection({
-        client: 'pg',
-        connection: {
-            database: process.env.DATABASE_NAME,
-            user: process.env.DATABASE_USERNAME,
-            password: process.env.DATABASE_PASSWORD,
-        },
-        migrations: {
-            tableName: 'knex_migrations',
-            directory: './migrations'
-        },
-        acquireConnectionTimeout: 10000,
-        pool: {
-            afterCreate: (conn:any, done:any) => {
-                conn.query('SET timezone="UTC";', (err:any)=>{
-                  if (err) {
-                    console.log(err)
-                  }
-                  done(err, conn)
-                })
-            }
-        },
-    },
-);
-export default knexConnect;
+import { knex } from 'knex';
+import { knexConfig } from './knexfile';
+
+export const db = knex(knexConfig);
