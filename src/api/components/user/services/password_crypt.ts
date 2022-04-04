2d0731b3d2131eaf09c53146e17d7e03099ace03
Andrés Ayelo Reig <diecitrece026@gmail.com>
2022-04-04 11:06:28 +0200

    fix: fixed password_hash() function again

--- src/api/components/user/services/password_crypt.ts
+++ src/api/components/user/services/password_crypt.ts
@@ -2,6 +2,6 @@ import bcrypt from 'bcrypt';
 export default async function password_crypt(password:string):Promise<string>
 {
     let salt = await bcrypt.genSalt();
-    let hash:string = await bcrypt.hash(password, salt);
+    let hash = await bcrypt.hash(password, salt);
     return hash;
 }
