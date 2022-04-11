23a4729b8de1e20416505e37bda57b559baddfa9
juanfril <juanfril@gmail.com>
2022-04-11 16:26:05 +0200

    test: pass first test

--- src/database/db-conection.spec.ts
+++ src/database/db-conection.spec.ts
@@ -2,29 +2,22 @@ import { checkUserExist } from './db-conection';
 
 describe('checkUserExist', () => {
   it('should return a user if user exist', async () => {
-    const login = {
-      email: 'juanfril@gmail.com',
-      password: '123456',
-    };
-    const user = {
-      id: '1',
-      firstName: 'John',
-      surNames: 'Doe',
-      email: 'juanfril@gmail.com',
-      password: '123456',
-      phone: '123456789',
-      address: 'Calle falsa 123',
-    };
-    const result = await checkUserExist(login.email);
-    expect(result).toEqual(user);
+    const result = await checkUserExist('juanfri@gmail.com');
+    expect(result).toEqual({
+      address: 'Calle mayor',
+      created_at: 2022-04-11T10:29:20.053Z,
+      email: 'juanfri@gmail.com',
+      firstName: 'jaufnri',
+      id: '1e75c522-7fb8-4d23-80ca-04785c7abd05',
+      password: '$2b$10$64pp0EB7IsVQoWNDMkDRpeHzVcuJMUVpvasySNZoQ0.xsMznp/yfu',
+      phone: '66611223344556',
+      surNames: 'vidal',
+      updated_at: 2022-04-11T10:29:20.053Z,
+    });
   });
 
   it('should return null if user does not exist', async () => {
-    const login = {
-      email: 'juanfril@gmail.com',
-      password: '123456',
-    };
-    const result = await checkUserExist(login.email);
+    const result = await checkUserExist('example@gmail.com');
     expect(result).toBeNull();
   });
 });
