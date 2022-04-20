5ae331c311060efe24a04384e3ab8402bc0a5433
Andrés Ayelo Reig <diecitrece026@gmail.com>
2022-04-20 10:17:52 +0200

    feat: user routes fixed

--- src/user-interface/user/user.routes.ts
+++ src/user-interface/user/user.routes.ts
@@ -1,10 +1,25 @@
 import User from '@domain/user';
 import router from '../../../src_original/api/components/user/user.routes';
-import { register } from './create-user.interface';
+import { schemaUserRegister } from './validate-user';
+import { generateId } from 'utils/uuid';
+import password_crypt from 'utils/password_crypt';
 import { Request, Response } from 'express';
+import { createUser } from '@use-cases/user/create-user.use-case';
 
-router.post('/api/users', (req: Request, res: Response) => {
-  const user: User = req.body;
-  const response = register(user);
-  res.send(response);
+router.post('/api/users', async (req: Request, res: Response) => {
+  const user: User = {
+    id : generateId(),
+    firstName : req.body.firstName,
+    surNames : req.body.surNames,
+    email: req.body.email,
+    password: req.body.password,
+    phone: req.body.phone,
+    address: req.body.address,
+  };
+  if (schemaUserRegister.validate(user).error) {
+    res.status(401).send(schemaUserRegister.validate(user).error?.details);
+  }
+  user.password = await password_crypt(user.password);
+  const newUser = await createUser(user);
+  res.status(200).json(newUser);
 });
