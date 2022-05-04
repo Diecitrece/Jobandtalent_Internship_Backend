6be223c275d30bc6529a0ddda33f66a0195f8293
juanfril <juanfril@gmail.com>
2022-05-04 13:14:52 +0200

    fix: PR request password_crypt

--- src/infrastructure/shared/password_crypt.spec.ts
+++ src/infrastructure/shared/password_crypt.spec.ts
@@ -1,14 +1,14 @@
 import { passwordCrypt } from '../../infrastructure/shared/password_crypt';
 
 describe('passwordCrypt', () => {
-  const password_crypt = passwordCrypt();
+  const { password_crypt, password_compare } = passwordCrypt();
 
   test('password_crypt', async () => {
-    expect(await password_crypt.password_crypt('123456')).toHaveLength(60);
+    expect(await password_crypt('123456')).toHaveLength(60);
   });
   test('password compare', async () => {
     expect(
-      await password_crypt.password_compare(
+      await password_compare(
         '123456',
         '$2b$10$RoTC8jHRNay.b0k.E7kH2urzd8rVsySmO7VtYYCz0DxO6UD7xf0DG'
       )
