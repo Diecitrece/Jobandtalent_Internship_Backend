19fd6aeb7d7e25d52d902999903b5e0c7253adaa
juanfril <juanfril@gmail.com>
2022-05-10 20:23:33 +0200

    fix: change name like others

---
+++ src/user-interface/user/user.routes.spec.ts
@@ -0,0 +1,27 @@
+import { User, UserRoles } from '../../core/domain/user.model';
+import { ReturnUserFormat, returnUserMapping } from './user.routes';
+describe('user.routes functions', () => {
+  test('Pass a user to this function, and then it returns it without password', () => {
+    const user: User = {
+      id: 'iduser',
+      firstName: 'Panceta',
+      surNames: 'Pancetapellido',
+      email: 'panceta@gmail.com',
+      password: 'panceta123',
+      phone: 'elnúmerodelapanceta',
+      address: 'calledelapanceta puerta 1 1ºC',
+      role: UserRoles.USER,
+    };
+    const result = returnUserMapping(user);
+    const expected: ReturnUserFormat = {
+      id: user.id,
+      firstName: user.firstName,
+      surNames: user.surNames,
+      email: user.email,
+      phone: user.phone,
+      address: user.address,
+      role: user.role,
+    };
+    expect(result).toStrictEqual(expected);
+  });
+});
